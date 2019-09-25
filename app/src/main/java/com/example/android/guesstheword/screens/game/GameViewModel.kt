cec58ed2c012091d728ab92668c2ea2f8363e48b
sr4p <srsai.sr4p@hotmail.com>
2019-09-25 17:49:44 +0700

    Populate viewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -4,6 +4,15 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
     init {
         Log.i("GameViewModel", "GameViewModel created!")
     }
@@ -12,4 +21,55 @@ class GameViewModel : ViewModel() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
     }
+
+    fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+     fun nextWord() {
+        if (!wordList.isEmpty()) {
+            //Select and remove a word from the list
+            word = wordList.removeAt(0)
+        }
+
+    }
+
+     fun onSkip() {
+        if (!wordList.isEmpty()) {
+            score--
+        }
+        nextWord()
+    }
+
+     fun onCorrect() {
+        if (!wordList.isEmpty()) {
+            score++
+        }
+        nextWord()
+    }
+
+
 }
